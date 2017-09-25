b9cb6483915e1da74ae81303c48b84cd95446743
ardenn <mc_alila@outlook.com>
2017-09-25 12:21:46 +0000

    Added SMPTlib Integration to send emails

--- dict.py
+++ dict.py
@@ -1,34 +1,37 @@
 import smtplib
-import email.utils
 from email.mime.text import MIMEText
 
 people = {}
 while True:
     name = input("Enter Name: ")
-    email = input("Enter Email: ")
+    e_mail = input("Enter Email: ")
     if name == "":
         break
-    people[name] = email
+    people[name] = e_mail
 
 
 def get_email(people_dict, name):
-    print(dict[name])
+    print(people_dict.get(name, "Sorry that person is not on our mailing list!"))
 
 
 def send_emails(people_dict):
-    for name, address in people_dict.items()
+    # my credentials
+    username = "rodgers.ouma@meltwater.org"
+    password = "nethunter7007&"
 
-    # Create the message
-    msg = MIMEText("Hello {}".format(name))
-    msg['To'] = email.utils.formataddr((name, address))
-    msg['From'] = email.utils.formataddr(("Anon User", 'cenafa@d1yun.com'))
-    msg['Subject'] = "Andrew's Python Class"
-
-    server = smtplib.SMTP('localhost', 1025)
+    # connect to server
+    server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    server.starttls()
+    server.login(username, password)
+    server.ehlo()
     server.set_debuglevel(True)  # show communication with the server
-    try:
-        server.sendmail('cenafa@d1yun.com',
-                        [address],
-                        msg.as_string())
-    finally:
-        server.quit()
+
+    for name, address in people_dict.items():
+        # create message
+        message = "Dear {}. How is your day?".format(name)
+        msg = MIMEText(message)
+        msg['Subject'] = "Andrew's Python Class"
+        msg['From'] = username
+        msg['To'] = address
+        server.sendmail(username, address, msg.as_string())
+    server.quit()
